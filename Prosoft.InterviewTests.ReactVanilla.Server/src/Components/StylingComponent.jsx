34bc4fdaa2a05851057512ee727af66e98ef37d1
Ted Chirvasiu <tedchirvasiu07@gmail.com>
2020-09-06 13:57:50 +0300

    Fixed color codes in StylingComponent.

--- Prosoft.InterviewTests.ReactVanilla.Server/src/Components/StylingComponent.jsx
+++ Prosoft.InterviewTests.ReactVanilla.Server/src/Components/StylingComponent.jsx
@@ -1,10 +1,10 @@
 ﻿import * as React from 'react';
 
 const metals = [
-    { name: "Brass", color: "b5a642" },
-    { name: "Copper", color: "b87333" },
-    { name: "Gold", color: "FFD700" },
-    { name: "Silver", color: "C0C0C0" },
+    { name: "Brass", color: "#b5a642" },
+    { name: "Copper", color: "#b87333" },
+    { name: "Gold", color: "#FFD700" },
+    { name: "Silver", color: "#C0C0C0" },
 ];
 
 export class StylingComponent extends React.Component {
